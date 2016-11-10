a37b11257913bc5f9520d7608028c7cc028d1ba0
Jeannie Yamakawa <jkyamakawa@gmail.com>
2016-11-10 13:30:03 -0600

    ui router hooked up

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,7 +1,45 @@
 var app = angular.module('redditApp', ['ui.router']);
+// console.log('booyah');
+app.config(function($stateProvider, $urlRouterProvider){
+    var homeState = {
+      name: 'home',
+      url: '/',
+      templateUrl: 'partials/signup.html',
+      
+    }
 
-app.config(function($stateProvider){
+    var userState = {
+      url: '/users',
+      templateUrl: 'partials/users.html',
+      controller: 'UserListController'
+    }
 
+    // var userDetailState = {
+    //   url: '/:user_name',
+    //   parent: userState,
+    //   templateUrl: 'partials/userDetail.html',
+    //   controller: 'UserDetailController'
+    // }
+    //
+    // var userFoodState = {
+    //   url: '/:food',
+    //   parent: userState,
+    //   templateUrl: 'partials/userDetail.html',
+    //   controller: 'UserDetailController'
+    // }
+    $stateProvider.state(homeState);
 
-    
+    // $stateProvider.state('users', userState);
+    // $stateProvider.state('users.detail', userDetailState);
+    // $stateProvider.state('users.food', userFoodState);
+
+    $urlRouterProvider.otherwise('/');
+  });
+
+    app.service('CurrentUserService', function() {
+    this.currentUser = {};
+    this.setCurrentUser = function(user) {
+      this.currentUser = user;
+    }
+
 })
